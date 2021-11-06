a91041b5f455ddda1990701b2225ec2a915934b1
yunlong.wang <ylwang9627@foxmail.com>
2021-11-06 13:49:07 +0800

    yl #000 implement @Value to set bean to miniIoC

--- src/main/java/mini/spring/ioc/MiniAnnotationConfigApplicationContext.java
+++ src/main/java/mini/spring/ioc/MiniAnnotationConfigApplicationContext.java
@@ -1,7 +1,9 @@
 package mini.spring.ioc;
 
 
+import mini.spring.ioc.annotation.Autowired;
 import mini.spring.ioc.annotation.Component;
+import mini.spring.ioc.annotation.Qualifier;
 import mini.spring.ioc.annotation.Value;
 import mini.spring.ioc.util.PackageUtil;
 import org.apache.commons.lang3.StringUtils;
@@ -44,10 +46,10 @@ public class MiniAnnotationConfigApplicationContext {
         Set<BeanDefinition> beanDefinitions = createBeanDefinitions(classesWithAnnotation);
 
         // set object to IoC
-        // 1.
+        // 1. handle @Value to create object, set to IoC
         createObjectBasedOnBeanDefinition(beanDefinitions);
-
-        // autowire bean for object
+        // 2. handle @autowired to autowire bean for object. get bean from IoC, and overwrite object
+        autowiredBeanForObject(beanDefinitions);
 
     }
 
@@ -90,41 +92,77 @@ public class MiniAnnotationConfigApplicationContext {
                 // get fields
                 Field[] declaredFields = beanClass.getDeclaredFields();
                 for (Field declaredField : declaredFields) {
-                    Value declaredFieldAnnotation = declaredField.getAnnotation(Value.class);
-                    if (declaredFieldAnnotation != null) {
-                        String value = declaredFieldAnnotation.value();
-
-                        // use set method to set value
-                        String setMethodName = "set" + StringUtils.capitalize(declaredField.getName());
-                        try {
-                            Method setMethod = beanClass.getMethod(setMethodName, declaredField.getType());
-
-                            // convert value type based on property type, because @Value return is String
-                            Object declaredValue = null;
-                            switch (declaredField.getType().getName()) {
-                                case "java.lang.String":
-                                    declaredValue = value;
-                                    break;
-                                case "java.lang.Integer":
-                                    declaredValue = Integer.parseInt(value);
-                                    break;
-                                default:
-                                    break;
-                            }
-
-                            setMethod.invoke(beanInstance, declaredValue);
-                        } catch (NoSuchMethodException | InvocationTargetException e) {
-                            logger.warning("Get method issue: " + e.getMessage());
-                            e.printStackTrace();
-                        }
-                    }
+                    // handle @Value
+                    handleValueAnnotation(beanClass, beanInstance, declaredField);
                 }
                 miniIoC.put(beanDefinition.getBeanName(), beanInstance);
             } catch (InstantiationException | IllegalAccessException e) {
                 e.printStackTrace();
             }
-
         });
+    }
+
+    private void handleValueAnnotation(Class beanClass, Object beanInstance, Field declaredField) throws IllegalAccessException {
+        Value declaredFieldAnnotation = declaredField.getAnnotation(Value.class);
+        if (declaredFieldAnnotation != null) {
+            String value = declaredFieldAnnotation.value();
+
+            // use set method to set value
+            String setMethodName = "set" + StringUtils.capitalize(declaredField.getName());
+            try {
+                Method setMethod = beanClass.getMethod(setMethodName, declaredField.getType());
+
+                // convert value type based on property type, because @Value return is String
+                Object declaredValue = null;
+                switch (declaredField.getType().getName()) {
+                    case "java.lang.String":
+                        declaredValue = value;
+                        break;
+                    case "java.lang.Integer":
+                        declaredValue = Integer.parseInt(value);
+                        break;
+                    default:
+                        break;
+                }
+                setMethod.invoke(beanInstance, declaredValue);
+            } catch (NoSuchMethodException | InvocationTargetException e) {
+                logger.warning("Get method issue: " + e.getMessage());
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    private void autowiredBeanForObject(Set<BeanDefinition> beanDefinitions) {
+    }
+
+
+    private void handleAutowiredAnnotation(Class beanClass, Object beanInstance, Field declaredField) {
+        Autowired autowired = declaredField.getAnnotation(Autowired.class);
+
+        if (autowired != null) {
+            Qualifier qualifier = declaredField.getAnnotation(Qualifier.class);
+            if (qualifier != null) {
+                // handle @Qualifier
+
+            } else {
+                String declaredFieldTypeName = declaredField.getType().getSimpleName();
+
+                System.out.println(declaredFieldTypeName);
+
+                String declaredFieldBeanName = StringUtils.uncapitalize(declaredFieldTypeName);
+                System.out.println(declaredFieldBeanName);
+                Object bean = getBean(declaredFieldBeanName);
+                System.out.println(bean);
+
+
+            }
+
+        }
+    }
+
+    public Object getBean(String declaredFieldBeanName) {
+        return miniIoC.get(declaredFieldBeanName);
     }
 
 }
