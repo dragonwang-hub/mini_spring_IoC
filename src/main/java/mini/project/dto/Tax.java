8fa16672d0e62c9d248852261d083106c675c233
yunlong.wang <ylwang9627@foxmail.com>
2021-11-06 12:01:48 +0800

    yl #000 add @Value annotation for object field

--- src/main/java/mini/project/dto/Tax.java
+++ src/main/java/mini/project/dto/Tax.java
@@ -2,10 +2,13 @@ package mini.project.dto;
 
 import lombok.Data;
 import mini.spring.ioc.annotation.Component;
+import mini.spring.ioc.annotation.Value;
 
 @Data
 @Component
 public class Tax {
+    @Value("SXAF0001")
     private String licensePlate;
+    @Value("BYD")
     private String carModal;
 }
--- src/main/java/mini/project/dto/TaxDriver.java
+++ src/main/java/mini/project/dto/TaxDriver.java
@@ -2,10 +2,13 @@ package mini.project.dto;
 
 import lombok.Data;
 import mini.spring.ioc.annotation.Component;
+import mini.spring.ioc.annotation.Value;
 
 @Data
 @Component
 public class TaxDriver {
+    @Value("9537")
     private Integer id;
+    @Value("Joey")
     private String name;
 }
